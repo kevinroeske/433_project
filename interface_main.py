908d9bb3d69d0e8848cbb387a760fe0684f55008
Kevin Roeske <kroeske@localhost.localdomain>
2018-02-25 00:54:02 -0800

    added token validation, token revocation, and the routine to extract customer data, decode it, and export it to the output target

--- interface_main.py
+++ interface_main.py
@@ -3,11 +3,13 @@ import Crypto
 from Crypto.Cipher import DES
 import hash_password
 import arrow
+import string
 
 salt ='' 
 key=''
 output_path=''
 data_path=''
+token_log = ''
 
 config_file = open("secrets/secrets.ini", 'r')
 
@@ -19,8 +21,10 @@ for line in config_file:
         key=data[1]
     if data[0] == 'data_path:':
         data_path=data[1]
-    if data[0] == 'output_path':
+    if data[0] == 'output_path:':
         output_path=data[1]
+    if data[0] == 'token_log:':
+        token_log=data[1]
 
 des = DES.new(key, DES.MODE_ECB)    #our encryption object, identical to the one in the encrypting script
 
@@ -52,15 +56,62 @@ def validate_pin(customer, pin):
 
 def generate_token(name):
 #
-#generates a session-specific token that acts as a signature and validation recipt for each
+#generates a session-specific token that grants access to data, and doubles as a signature and validation recipt for each
 #instance of data access
 #
+    time = arrow.get().isoformat()
     token = {}
     id_number = ''
-    for index in range(128):
-        id_number += random.choice(string.printable)
-    token['id'] = id_number
+    for index in range(16):
+        id_number += random.choice(string.hexdigits)
+    token['id'] = "0x"+id_number
     token['time'] = arrow.get().isoformat()
     token['valid'] = True
     token['customer'] = name
+    token_record = open(token_log, "a+")
+    token_record.write("Token# " + token['id'] + "\n")
+    token_record.write("Issued at " + time + "\n")
+    token_record.write("For customer: " + name + "\n")
+    token_record.close()
     return token
+
+def validate_token(token):
+    token_record = open(token_log, "r")
+    data = token_record.read()
+    token_record.close()
+    return (token['id'] in data and token['id'] + " invalidated" not in data and token['valid'] == True)
+
+def invalidate_token(token):
+    time = arrow.get().isoformat()
+    token['valid'] = False
+    token_record = open(token_log, "a+")
+    token_record.write("Token " + token['id'] + " invalidated at " + time + "\n\n\n")
+    token_record.close()
+
+def get_data(customer, data_type, token):
+    if not (validate_token(token)) or customer != token['customer']:
+        return
+    time = arrow.get().isoformat()
+    customer_data = open(data_path, "r")
+    line = ''
+    while customer not in line:
+        line = customer_data.readline()
+    while data_type not in line:
+        line = customer_data.readline()
+    customer_data.close()
+    parts = line.split()
+    encoded_data = parts[1]
+    output_string = "Your " + data_type + " is " + des.decrypt(encoded_data) + "\n"
+    token_string = "Accessed using token " + token['id'] + " at " + time + "\n"
+    customer_name = customer.split()[0] + "_" + customer.split()[1]
+    output_file = open(output_path + "/" + customer_name + ".log", "a+")
+    output_file.write(output_string)
+    output_file.write(token_string)
+    output_file.close()
+
+token = generate_token("John Brown")
+print(str(validate_token(token)))
+get_data("John Brown", "Balance", token)
+invalidate_token(token)
+get_data("John Brown", "Balance", token)
+print(str(validate_token(token)))
